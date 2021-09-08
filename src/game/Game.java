d388d04bbdba484e1d2c36ba63918fbc1c6146cb
pupil <pupil@ivkhk.ee>
2021-09-08 11:23:33 +0300

    Second commit

--- src/game/Game.java
+++ src/game/Game.java
@@ -9,6 +9,9 @@ package game;
  *
  * @author pupil
  */
+import java.util.Random;
+import java.util.Scanner;
+
 public class Game {
 
     /**
@@ -16,6 +19,14 @@ public class Game {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Random random = new Random();
+        Scanner scanner = new Scanner(System.in);
+        int number = random.nextInt(9-0+1)+0;
+        System.out.println("Find a right number!");
+        int userNumber = scanner.nextInt();
+        if(number == userNumber) {System.out.println("You won!");}
+        else {System.out.println("no, the number is: "+number);}
+        }
     }
     
 }
