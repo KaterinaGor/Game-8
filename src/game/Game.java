9474daf33548f3ccd998af60224500c1e4a176de
pupil <pupil@ivkhk.ee>
2021-09-13 09:45:04 +0300

    Circle added

--- src/game/Game.java
+++ src/game/Game.java
@@ -18,14 +18,23 @@ public class Game {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int attempt = 0;
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
         int number = random.nextInt(9-0+1)+0;
         System.out.println("Find a right number!");
-        int userNumber = scanner.nextInt();
-        if(number == userNumber) {System.out.println("You won!");}
-        else {System.out.println("no, the number is: "+number);}
+        for (int i = 0; i < 3; i++) {
+            int userNumber = scanner.nextInt();
+            if(number == userNumber) {System.out.println("You won!"); 
+            break;
+            } else {
+                if(attempt < 2){
+                System.out.println("Try again!");
+                } else {
+                System.out.println("You lose!");
+                }
+            }
+            attempt++;
         }
     }
     
